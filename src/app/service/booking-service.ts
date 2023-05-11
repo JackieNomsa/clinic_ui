f4e9b849ab0191a9e4784d82e9359f2309b74e7d
jackie dube <jackienomsa3@gmail.com>
2023-05-11 19:14:47 +0200

    service

---
+++ src/app/service/booking-service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { BookingModel } from '../model/booking-model';
+
+@Injectable()
+export class BookingService{
+    public url:string = "http://localhost:8080/clinic"
+
+    constructor(private http: HttpClient) {
+
+     }
+
+    public bookASlot(bookingData: BookingModel) {
+        return this.http.post(`${this.url}/create`,bookingData);
+    }
+
+    
+    public getBooking(id: string) {
+        return this.http.get<BookingModel>(`${this.url}/getById/${id}`);
+    }
+}
